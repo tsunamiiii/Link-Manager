f27cf80f536b8439def89a343cdcf53599c01325
Waves <creepykid1101@gmail.com>
2019-01-19 02:58:50 -0600

    Update LM.js

--- LM.js
+++ LM.js
@@ -6,7 +6,7 @@ bot.channels.get('536088775600898048').send('Link Manager v1.0 Operational');
 });
 
 bot.on("message", message => {
-if(message.content.toLowerCase.includes("https://youtu")) {
+if(message.content.toLowerCase()0.includes("https://youtu")) {
 if(message.channel.id == '536097338628767754') return;
 message.delete();
 } 
