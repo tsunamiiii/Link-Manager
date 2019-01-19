d2ebae95ecfd92927377e569ff68d2ca0a2e095e
Waves <creepykid1101@gmail.com>
2019-01-19 02:44:38 -0600

    Add files via upload

---
+++ LM.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.on("ready", () => {
+bot.channels.get('536088775600898048').send('Link Manager v1.0 Operational');
+});
+
+bot.on("message", message => {
+if(message.content.toLowerCase.includes("https://youtu") {
+if(message.channel.id == '536097338628767754') return;
+message.delete();
+} 
+});
+
+bot.login(process.env.TOKEN)
