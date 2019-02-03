9bdb289de430e3ad11b76a976754694f04d859d7
Waves <creepykid1101@gmail.com>
2019-02-02 18:39:38 -0800

    Update LM.js

--- LM.js
+++ LM.js
@@ -13,7 +13,7 @@ let msg = message.content.toUpperCase();
 let args = message.content.slice(pfix.length).trim().split(' ');
 let cmd = args.shift().toLowerCase();
 if(message.author.bot) return;
-if(msg.includes('.YOUTU')) {
+if(msg.includes('HTTPS' && 'YOUTU')) {
    if(message.channel.id == '536097338628767754') return;
    message.delete();
    message.reply('<#536097338628767754>').then(msg => {
