13ae5936ca1c42cf28e5486cfd80c6ba21d056f4
Waves <creepykid1101@gmail.com>
2019-02-02 18:24:51 -0800

    Update LM.js

--- LM.js
+++ LM.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const pfix = "yt;";
+const pfix = "YT;";
 
 bot.on("ready", () => {
 console.log('Im managing again!');
@@ -8,13 +8,19 @@ bot.channels.get('536088775600898048').send('Link Manager v1.3.1 Operational');
 bot.user.setActivity(`you | yt;help`, {type: "LISTENING"});
 });
 
-bot.on('message', message => {
+bot.on('message', async message => {
 let msg = message.content.toUpperCase();
 let args = message.content.slice(pfix.length).trim().split(' ');
 let cmd = args.shift().toLowerCase();
 if (!msg.startsWith(pfix)) return;
 if(message.author.bot) return;
-
+if(msg.includes('.youtu') {
+   if(message.channel.id == '536097338628767754') return;
+   message.delete();
+   message.reply('<#536097338628767754>').then(msg => {
+     msg.delete(5000)
+   });
+}
 try{
 let commandFile = require(`./commands/${cmd}.js`);
 commandFile.run(bot, message, args);
